1325d59cc6c204232c49b3d00387f8d0e7976cf0
Malek KHIRI <uid1345@focus.com.tn>
2021-12-16 08:49:21 +0100

    dashboard contracts cards

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,6 +5,16 @@ import { Route, RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { TranslocoModule } from '@ngneat/transloco';
 import { PubComponent } from '../pub/pub.component';
+import { FuseCardModule } from '@fuse/components/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 const dashboardRoutes: Route[] = [
   {
@@ -22,7 +32,19 @@ const dashboardRoutes: Route[] = [
     CommonModule,
     RouterModule.forChild(dashboardRoutes),
     SharedModule,
-    TranslocoModule
+    TranslocoModule,
+    FuseCardModule,
+
+    MatButtonModule,
+    //MatButtonToggleModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatTooltipModule,
   ]
 })
 export class DashboardModule { }
